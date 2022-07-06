32e67e5c2597676286332abd1b59e94c02e8d2db
marvinscheffold <m@marvinscheffold.com>
2022-07-06 14:26:41 +0200

    solution

--- solution.js
+++ solution.js
@@ -1,1 +1,65 @@
+// 1
+const euroCities = ["Paris", "London", "Valletta", "Prag", "Rom"];
 
+const secondCity = euroCities[1];
+
+// 2
+euroCities[0] = "Berlin";
+
+// 3
+console.log(euroCities.length);
+
+// 4
+euroCities.pop();
+console.log(euroCities);
+
+// 5
+euroCities.push("Budapest");
+console.log("euroCities", euroCities);
+
+// 6
+const asianCities = ["Kyoto", "Delhi", "Wuhan", "Shanghai", "Osaka"];
+//console.log(asianCities);
+
+// 7
+const worldCities = euroCities.concat(asianCities);
+//console.log(worldCities);
+
+// 8
+worldCities.reverse();
+//sconsole.log(worldCities);
+
+/**
+ * BONUS!
+ */
+
+// 9
+/**
+ * splice(wo soll etwas passieren, wieviele sollen gelöscht werden)
+ *
+ * splice verändert das Ursprungsarray
+ */
+
+euroCities.splice(2, 1);
+console.log("euroCities", euroCities);
+
+// 10
+/**
+ * slice(von wo, bis wo)
+ * slice verändert das Ursprungsarray nicht
+ */
+const newAsianCities = asianCities.slice(1, 4);
+
+// 11
+
+// Option 1:
+// worldCities[2] = "Toronto"
+// Option 2:
+worldCities.splice(2, 1, "Toronto");
+
+// 12
+
+worldCities.splice(1, 0, "Washington");
+
+// 13
+console.log(worldCities.join(" + "));
